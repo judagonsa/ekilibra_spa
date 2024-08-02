5dbe9038e547f541529ccf5bf0a30431b37b9c13
judagonsa <judagonsa8@gmail.com>
2024-08-01 20:55:23 -0500

    Create custom form text filds

--- lib/app/pages/profile/pages/profile_page.dart
+++ lib/app/pages/profile/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:ekilibra_spa/app/pages/profile/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,16 +12,72 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(isRegister ? 'Registro' : 'Mi perfil'),
       ),
-      body: const _ProfileView(),
+      body: _ProfileView(isRegister: isRegister),
     );
   }
 }
 
 class _ProfileView extends StatelessWidget {
-  const _ProfileView();
+  const _ProfileView({required this.isRegister});
 
+  final bool isRegister;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: _InputForm(isRegister: isRegister),
+        ),
+      ),
+    );
+  }
+}
+
+class _InputForm extends StatelessWidget {
+  const _InputForm({required this.isRegister});
+
+  final bool isRegister;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [
+        Column(
+          children: [
+            if (isRegister) const FlutterLogo(size: 200),
+            CustomTextFormField(
+              label: 'Nombre y apellido',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormField(
+              label: 'Correo',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormField(
+              label: 'Fecha de nacimiento',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormField(
+              label: 'Contrseña',
+              obscureText: true,
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormField(
+              label: 'Confirmar contraseña',
+              obscureText: true,
+            ),
+            const SizedBox(height: 10),
+            FilledButton.tonalIcon(
+                onPressed: () {},
+                label: Text(isRegister ? 'Crear usuario' : 'Guardar')),
+            const SizedBox(height: 20)
+          ],
+        ),
+      ],
+    ));
   }
 }
---
+++ lib/app/pages/profile/widgets/custom_text_form_field.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  CustomTextFormField({
+    super.key,
+    this.label,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+    required this.obscureText,
+  });
+
+  final String? label;
+  final String? errorMessage;
+  final Function(String?)? onChanged;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  final border = OutlineInputBorder(borderRadius: BorderRadius.circular(10));
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder:
+            border.copyWith(borderSide: const BorderSide(color: Colors.red)),
+        isDense: true,
+        labelStyle: const TextStyle(color: Colors.black),
+        focusColor: Colors.black,
+        focusedErrorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        errorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        label: label != null ? Text(label!) : null,
+        errorText: errorMessage,
+      ),
+    );
+  }
+}
