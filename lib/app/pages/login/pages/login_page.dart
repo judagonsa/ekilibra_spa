bce625cce98d958ab88bf656afc8619ac1c036c5
judagonsa <judagonsa8@gmail.com>
2024-08-12 08:24:14 -0500

    Add form and validate fields

--- lib/app/pages/login/pages/login_page.dart
+++ lib/app/pages/login/pages/login_page.dart
@@ -14,84 +14,94 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formLoginKey = GlobalKey<FormState>();
+    bool isRealtime = false;
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: Scaffold(
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 30),
-                  child: Image(
-                    image: AssetImage('assets/images/logo.png'),
-                    height: 200,
-                    width: 200,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: CustomTextFormField(
-                    label: 'Número de teléfono',
-                    // textEditingController: _controllerName,
-                    onChanged: (value) {
-                      // profileCubit.usernameChanged(value);
-                      // if (isRealtime) _formKey.currentState?.validate();
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'El número teléfonico es requerido.';
-                      }
-                      if (value.length != 10) {
-                        return 'Número teléfonico incorrecto.';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: CustomTextFormField(
-                    label: 'Contraseña',
-                    // obscureText: obscureTextPassword,
-                    iconInput: Icon(
-                      obscureTextPassword
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    iconActtion: () {
-                      setState(() {
-                        obscureTextPassword = !obscureTextPassword;
-                      });
-                    },
-                    onChanged: (value) {
-                      // profileCubit.passwordChanged(value);
-                      // if (isRealtime) _formKey.currentState?.validate();
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'La contraseña es requerida.';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: TextButton(
-                    onPressed: () {
-                      FocusScope.of(context).requestFocus(FocusNode());
-                    },
-                    style: ButtonHelpers().primaryButton(),
-                    child: const Text(
-                      'Iniciar sesión',
-                      style: TextStyle(fontSize: 16),
+            child: Form(
+              key: _formLoginKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
+                      height: 200,
+                      width: 200,
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: CustomTextFormField(
+                      label: 'Número de teléfono',
+                      // textEditingController: _controllerName,
+                      onChanged: (value) {
+                        // profileCubit.usernameChanged(value);
+                        if (isRealtime) _formLoginKey.currentState?.validate();
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'El número teléfonico es requerido.';
+                        }
+                        if (value.length != 10) {
+                          return 'Número teléfonico incorrecto.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: CustomTextFormField(
+                      label: 'Contraseña',
+                      // obscureText: obscureTextPassword,
+                      iconInput: Icon(
+                        obscureTextPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      iconActtion: () {
+                        setState(() {
+                          obscureTextPassword = !obscureTextPassword;
+                        });
+                      },
+                      onChanged: (value) {
+                        // profileCubit.passwordChanged(value);
+                        if (isRealtime) _formLoginKey.currentState?.validate();
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'La contraseña es requerida.';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: TextButton(
+                      onPressed: () {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                        isRealtime = true;
+                        final isValid = _formLoginKey.currentState!.validate();
+                        if (isValid) {
+                          print('revisar datos on firebase');
+                        }
+                      },
+                      style: ButtonHelpers().primaryButton(),
+                      child: const Text(
+                        'Iniciar sesión',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
