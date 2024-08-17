ec503ddf5cd54ab0e567a0bf7e921fef7fcd37c1
judagonsa <judagonsa8@gmail.com>
2024-08-16 19:23:44 -0500

    Show days in spanish

--- lib/app/config/helpers/datetime_helper.dart
+++ lib/app/config/helpers/datetime_helper.dart
@@ -6,14 +6,25 @@ class DatetimeHelper {
   List<DateTime> getDaysOfWeek() {
     List<DateTime> days = [];
     for (var i = 0; i < 7; i++) {
-      days.add(DateTime.now().add(Duration(days: i)));
+      if (DateTime.now().add(Duration(days: i)).weekday != 6) {
+        days.add(DateTime.now().add(Duration(days: i)));
+      }
     }
-    //remover sábado
     return days;
   }
 
   String getDayString(DateTime date) {
-    return DateFormat('EEEE, d').format(date);
+    final Map<String, String> dayWeekArray = {
+      'Monday': 'Lunes',
+      'Tuesday': 'Martes',
+      'Wednesday': 'Miércoles',
+      'Thursday': 'Jueves',
+      'Friday': 'Viernes',
+      'Sunday': 'Sábado',
+    };
+    final dayOfWeek = DateFormat('EEEE').format(date);
+    final numberOfWeek = DateFormat('d').format(date);
+    return '${dayWeekArray[dayOfWeek]}, $numberOfWeek';
   }
 
 //función para retornar las horas a partir de la actual, hasta las 6 pm cada hora o cada 30
